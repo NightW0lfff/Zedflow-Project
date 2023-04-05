f133936690e30eb02b8de30fbd115a0d0aeb0100
NightW0lfff <philong170101@gmail.com>
2023-04-05 18:48:48 +1000

    Navigation Bar Initialise
    Added basic html Navigation bar into main page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import "./style.css";
+
 function App() {
-  return <h1>Zedflow Project</h1>;
+  const style = {
+    display: "none",
+  };
+
+  return (
+    <nav className="nav">
+      <a href="/" className="site-tile">
+        <h1>Zedflow Project</h1>
+        <ul>
+          <li>
+            <a href="/" id="product-dropdown">
+              Product
+            </a>
+            <div id="product-list" style={style}>
+              <ul>
+                <li>Product 1 - $10.00</li>
+                <li>Product 2 - $15.00</li>
+                <li>Product 3 - $20.00</li>
+              </ul>
+            </div>
+          </li>
+          <li>Stock</li>
+          <li>Sale</li>
+          <li>Variant</li>
+          <li>Trademark</li>
+          <li>Keyword</li>
+          <li>Artist</li>
+          <li>Category</li>
+          <li>Performance</li>
+          <li>Setting</li>
+        </ul>
+      </a>
+    </nav>
+  );
 }
 
 export default App;
