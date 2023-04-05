f51f4465a4c6c645847e25e193e244edda22d622
NightW0lfff <philong170101@gmail.com>
2023-04-05 22:43:31 +1000

    Add brand logo and some CSS style

--- src/App.js
+++ src/App.js
@@ -2,37 +2,37 @@ import "./style.css";
 
 function App() {
   const style = {
-    display: "none",
+    display: "block",
   };
 
   return (
     <nav className="nav">
       <a href="/" className="site-tile">
-        <h1>Zedflow Project</h1>
-        <ul>
-          <li>
-            <a href="/" id="product-dropdown">
-              Product
-            </a>
-            <div id="product-list" style={style}>
-              <ul>
-                <li>Product 1 - $10.00</li>
-                <li>Product 2 - $15.00</li>
-                <li>Product 3 - $20.00</li>
-              </ul>
-            </div>
-          </li>
-          <li>Stock</li>
-          <li>Sale</li>
-          <li>Variant</li>
-          <li>Trademark</li>
-          <li>Keyword</li>
-          <li>Artist</li>
-          <li>Category</li>
-          <li>Performance</li>
-          <li>Setting</li>
-        </ul>
+        <img src="zedflow-logo.png" alt="Zedflow Logo" className="logo" />
       </a>
+      <ul>
+        <li>
+          <a href="/" id="nav-item product-dropdown">
+            Product
+          </a>
+          <div id="nav-item product-list" style={style}>
+            <ul>
+              <li>Product 1</li>
+              <li>Product 2</li>
+              <li>Product 3</li>
+            </ul>
+          </div>
+        </li>
+        <li>Stock</li>
+        <li>Sale</li>
+        <li>Variant</li>
+        <li>Trademark</li>
+        <li>Keyword</li>
+        <li>Artist</li>
+        <li>Category</li>
+        <li>Performance</li>
+        <li>Setting</li>
+      </ul>
     </nav>
   );
 }
