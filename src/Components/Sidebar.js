453480131a4d0231601b31b5f9bc849d54d590a5
NightW0lfff <philong170101@gmail.com>
2023-04-11 05:02:28 +1000

    Updated Code
    Add drop-down feature and arrow rotating.

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,59 +1,95 @@
 import "../style.css";
 import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
 
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 function Sidebar(props) {
-  // const [state, setState] = useState(false);
+  const path = useLocation();
+  const [sublistVisible, setSublistVisible] = useState(false);
+  const [arrowRotation, setArrowRotation] = useState(false);
+
+  function toggleSublist(index) {
+    setSublistVisible(sublistVisible === index ? false : index);
+  }
+
   return (
     <div className="sidebar">
       <div className="sidebar-header">
         <a href="/Home" className="sidebar-logo">
-          <img src="zedflow-logo.png" alt="Zedflow Logo" className="logo" />
+          <img src="/zedflow-logo.png" alt="Zedflow Logo" className="logo" />
         </a>
       </div>
       <ul className="sidebar-list">
         {SidebarData.map((value, key) => {
+          const isActive = path.pathname.includes(value.path);
           return (
             <li
               key={key}
               className={`row ${value.hasSubList ? "" : "no-arrow"}`}
-              onClick={() => {
-                window.location.pathname = value.path;
-              }}
             >
-              <div
-                className="row list-container"
-                id={
-                  window.location.pathname.includes(value.path) ? "active" : ""
-                }
-              >
-                <div id="icon">{value.icon}</div>
-                <div id="title">{value.title}</div>
-                {value.hasSubList && <KeyboardArrowRightIcon id="arrow" />}
-              </div>
+              {!value.hasSubList ? (
+                <Link
+                  to={value.path}
+                  className="row no-arrow list-container"
+                  id={isActive && "active"}
+                >
+                  <div className="row no-arrow list-container">
+                    <div id="icon">{value.icon}</div>
+                    <div id="title">{value.title}</div>
+                  </div>
+                </Link>
+              ) : (
+                <Link
+                  to={value.path}
+                  className="row list-container"
+                  id={isActive && "active"}
+                >
+                  <div
+                    className="row list-container"
+                    onClick={() => toggleSublist(key)}
+                    id={isActive && "active"}
+                  >
+                    <div id="icon">{value.icon}</div>
+                    <div id="title">{value.title}</div>
+                    {value.hasSubList && (
+                      <KeyboardArrowRightIcon
+                        id="arrow"
+                        onClick={() => {
+                          toggleSublist(key);
+                          setArrowRotation(!arrowRotation);
+                        }}
+                        className={sublistVisible === key ? "rotate" : ""}
+                      />
+                    )}
+                  </div>
+                </Link>
+              )}
+
               {value.hasSubList && (
-                <ul>
+                <ul
+                  className={`sublist ${
+                    sublistVisible === key ? false : "hidden"
+                  }`}
+                >
                   {value.subList.map((subValue, subKey) => {
+                    const isActive = path.pathname.includes(subValue.path);
                     return (
                       <li
                         key={subKey}
-                        className="sublist"
-                        onClick={() => {
-                          window.location.pathname = subValue.path;
-                        }}
+                        className="row sublist"
+                        // id={isActive && "active"}
                       >
-                        <div
-                          className="row sublist-container"
-                          id={
-                            window.location.pathname.includes(subValue.path)
-                              ? "active"
-                              : ""
-                          }
-                        >
-                          <div id="title">{subValue.title}</div>
-                        </div>
+                        <Link to={subValue.path}>
+                          <div
+                            className="row sublist-container"
+                            id={isActive && "active"}
+                          >
+                            <div></div>
+                            <div id="title">{subValue.title}</div>
+                          </div>
+                        </Link>
                       </li>
                     );
                   })}
--- src/Components/SidebarData.js
+++ src/Components/SidebarData.js
@@ -38,39 +38,132 @@ const SidebarData = [
     title: "Stock",
     icon: <InventoryIcon />,
     path: "/Stock",
-    // hasSubList: true,
-    hasSubList: false,
+    hasSubList: true,
+    subList: [
+      {
+        title: "Stock On Hand",
+        path: "/stock/stock-on-hand",
+      },
+      {
+        title: "Stock Reorder",
+        path: "/stock/stock-reorder",
+      },
+    ],
   },
   {
     title: "Sales",
     icon: <ShoppingCartIcon />,
     path: "/Sales",
-    hasSubList: false,
-    // hasSubList: true,
+    hasSubList: true,
+    subList: [
+      {
+        title: "Customer",
+        path: "/sales/customer",
+      },
+      {
+        title: "Sales Report",
+        path: "/sales/sales-report",
+      },
+    ],
   },
   {
     title: "Variants",
     icon: <LibraryBooksIcon />,
-    path: "/Variants",
-    // hasSubList: true,
-    hasSubList: false,
+    path: "/variants",
+    hasSubList: true,
+    subList: [
+      {
+        title: "T-shirt",
+        path: "/variants/t-shirt",
+        hasSubList: false,
+      },
+      {
+        title: "Mugs",
+        path: "/variants/mugs",
+        hasSubList: false,
+      },
+      {
+        title: "Tote Bags",
+        path: "/variants/totebags",
+        hasSubList: false,
+      },
+      {
+        title: "Cushion Cover",
+        path: "/variants/cushion-cover",
+        hasSubList: false,
+      },
+      {
+        title: "Hoodies",
+        path: "/variants/hoodies",
+        hasSubList: false,
+      },
+      {
+        title: "Kids",
+        path: "/variants/kids",
+        hasSubList: false,
+      },
+      {
+        title: "Features",
+        path: "/variants/features",
+        hasSubList: true,
+        subList: [
+          {
+            title: "Image",
+            path: "/variants/features/image",
+            hasSubList: false,
+          },
+          {
+            title: "Price",
+            path: "/variants/features/price",
+            hasSubList: false,
+          },
+          {
+            title: "Merchant Price",
+            path: "/variants/features/merchant-price",
+            hasSubList: false,
+          },
+          {
+            title: "Description",
+            path: "/variants/features/description",
+            hasSubList: false,
+          },
+        ],
+      },
+    ],
   },
   {
     title: "TradeMarks",
     icon: <BookIcon />,
-    path: "/TradeMarks",
-    hasSubList: false,
+    path: "/trademarks",
+    hasSubList: true,
+    subList: [
+      {
+        title: "Approved Keywords",
+        path: "/trademarks/approved-key-words",
+        hasSubList: false,
+      },
+      {
+        title: "Banned Keywords",
+        path: "/trademarks/banned-key-words",
+        hasSubList: false,
+      },
+      {
+        title: "Class Words",
+        path: "/trademarks/class-words",
+        hasSubList: false,
+      },
+    ],
   },
   {
     title: "Keywords",
     icon: <InboxIcon />,
-    path: "/Keywords",
+    path: "/keywords",
     hasSubList: false,
   },
   {
     title: "Artist",
     icon: <SchoolIcon />,
-    path: "/Artist",
+    path: "/artist",
     hasSubList: false,
   },
   {
@@ -89,8 +182,19 @@ const SidebarData = [
     title: "Setting",
     icon: <SettingsIcon />,
     path: "/Setting",
-    hasSubList: false,
-    // hasSubList: true,
+    hasSubList: true,
+    subList: [
+      {
+        title: "General",
+        path: "/setting/general",
+        hasSubList: false,
+      },
+      {
+        title: "Esty Setting",
+        path: "/setting/esty-setting",
+        hasSubList: false,
+      },
+    ],
   },
 ];
 
