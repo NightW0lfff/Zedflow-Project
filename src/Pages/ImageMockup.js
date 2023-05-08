02d1b8b0610e3f330afc63b444a585ab4914bc6a
NightW0lfff <philong170101@gmail.com>
2023-05-08 23:42:39 +1000

    Update ImageMockup.js

--- src/Pages/ImageMockup.js
+++ src/Pages/ImageMockup.js
@@ -7,30 +7,70 @@ function ImageMockup() {
       <Main className="image-mockup-container">
         <h1 className="product-image title">Product Image</h1>
         <div className="product-image buttons">
-          <button className="btn original-pdf">Original PDF</button>
-          <button className="btn light-pdf">For Light</button>
-          <button className="btn dark-pdf">For Dark</button>
-          <button className="btn sticker-pdf">Stickers PDF</button>
-          <button className="btn sticker-png">Stickers PNG</button>
-          <button className="btn create">Create</button>
+          <button>Original PDF</button>
+          <button>For Light</button>
+          <button>For Dark</button>
+          <button>Stickers PDF</button>
+          <button>Stickers PNG</button>
+          <button>Create</button>
         </div>
-        <div className="product-image S&Scontainer">
+        <div className="product-image-SScontainer">
           <div className="product-image shirts">
             <h3>Shirts</h3>
-            <div className="shirt-logo-container">
-              <div className="shirt-light-logo">
-                <title className="shirt-light-logo-title">Adult Tees</title>
-                <div className="shirt"></div>
+            <div className="shirt-card-container">
+              <div className="shirt-logo-container">
+                <div className="image-box" src="" alt="" />
+                <div className="image-btn">
+                  <button>Upload Image</button>
+                  <button>Remove Image</button>
+                </div>
               </div>
-              <div className="shirt-dark-logo"></div>
+              <div className="shirt-logo-container">
+                <div className="image-box" src="" alt="" />
+                <div className="image-btn">
+                  <button>Upload Image</button>
+                  <button>Remove Image</button>
+                </div>
+              </div>
+              <div className="product-image-box mockup" src="" alt="" />
+              <div className="product-image-box mockup" src="" alt="" />
             </div>
+            <span className="input-container">
+              <input type="checkbox" />
+              Upload
+            </span>
           </div>
+          <div className="product-image-container"></div>
           <div className="product-image stickers">
             <h3>Stickers</h3>
           </div>
         </div>
         <div className="product-image mugs">
-          <h3>Mugs</h3>
+          <div className="product-image shirts">
+            <h3>Mugs</h3>
+            <div className="shirt-card-container">
+              <div className="shirt-logo-container">
+                <div className="image-box" src="" alt="" />
+                <div className="image-btn">
+                  <button>Upload Image</button>
+                  <button>Remove Image</button>
+                </div>
+              </div>
+              <div className="shirt-logo-container">
+                <div className="image-box" src="" alt="" />
+                <div className="image-btn">
+                  <button>Upload Image</button>
+                  <button>Remove Image</button>
+                </div>
+              </div>
+              <div className="product-image-box mockup" src="" alt="" />
+              <div className="product-image-box mockup" src="" alt="" />
+            </div>
+            <span className="input-container">
+              <input type="checkbox" />
+              Upload
+            </span>
+          </div>
         </div>
       </Main>
     </div>
