e15906c438cd62ae3829e8c877f14e53c1a25683
hungTDH1998 <49639660+hungTDH1998@users.noreply.github.com>
2023-05-24 16:32:29 +1000

    fixing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import "./style.css";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
-import ProductCreate from "./Pages/ProductCreate.js";
+// import ProductCreate from "./Pages/ProductCreate.js";
 import Register from "./Pages/Register.js";
 import ProductList from "./Pages/ProductList";
 import ImageMockup from "./Pages/ImageMockup";
@@ -13,9 +13,10 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
-        <Route path="/products/create" element={<ImageMockup />} />
+        {/* <Route path="/products/create" element={<ImageMockup />} /> */}
         <Route path="/register" element={<Register />}></Route>
         <Route path="/products/list" element={<ProductList />}></Route>
+        <Route path="/products/create" element={<ImageMockup />} />
       </Routes>
     </div>
   );
