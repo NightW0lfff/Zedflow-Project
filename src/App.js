e71de8fed45f02a9e2f8106c3c55f8c5fa2ab31b
hungTDH1998 <49639660+hungTDH1998@users.noreply.github.com>
2023-05-25 22:15:49 +1000

    fixing css bugs

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./style.css";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 import ImageMockup from "./Pages/ImageMockup";
+import Register from "./Pages/Register.js";
+import ProductList from "./Pages/ProductList/index"
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />}></Route>
         <Route path="/products/create" element={<ImageMockup />} />
+        <Route path="/products/list" element={<ProductList />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
