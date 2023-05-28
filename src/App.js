fd6e87e3a22c8b7f1cda43f2196e67bcdd83a219
SimonnaXY <129708915+SimonnaXY@users.noreply.github.com>
2023-05-28 21:28:17 +0800

    Update navigation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./Pages/Home";
 import ProductCreate from "./Pages/ProductCreate";
 import Tshirtvariant from "./Pages/Tshirtvariant";
 import Mugvariant from "./Pages/Mugvariant";
+import PCsecond from "./Pages/PCsecond";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/products/create" element={<ProductCreate />}></Route>
         <Route path="/variant/t-shirt" element={<Tshirtvariant />}></Route>
         <Route path="/variant/mug" element={<Mugvariant />}></Route>
+        <Route path="/pcsecond" element={<PCsecond />}></Route>
       </Routes>
     </div>
   );
