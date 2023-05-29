c89794c67dd34f1c3520ce6877c1ba8de6542172
hungTDH1998 <49639660+hungTDH1998@users.noreply.github.com>
2023-05-29 15:54:47 +1000

    update some

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./style.css";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 import Reset from "./Pages/Reset.js";
-import ProductCreate from "./Pages/ProductCreate.js";
 import Register from "./Pages/Register.js";
+import ProductList from "./Pages/ProductList/index"
+import ImageMockup from "./Pages/ImageMockup";
 
 function App() {
   return (
@@ -13,9 +13,10 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
-        <Route path="/products/create" element={<ProductCreate />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/reset" element={<Reset />} />
+        <Route path="/products/list" element={<ProductList  />}></Route>
+        <Route path="/products/create" element={<ImageMockup />} />
       </Routes>
     </div>
   );
