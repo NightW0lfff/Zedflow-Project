9022dd86ea639460eb040225481860e65fbdd1e4
hungTDH1998 <49639660+hungTDH1998@users.noreply.github.com>
2023-05-29 14:19:08 +1000

    fixing login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./style.css";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
-import ImageMockup from "./Pages/ImageMockup";
+import Reset from "./Pages/Reset.js";
+import ProductCreate from "./Pages/ProductCreate.js";
 import Register from "./Pages/Register.js";
-import ProductList from "./Pages/ProductList/index"
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/products/create" element={<ImageMockup />} />
-        <Route path="/products/list" element={<ProductList />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/products/create" element={<ProductCreate />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/reset" element={<Reset />} />
       </Routes>
     </div>
   );
