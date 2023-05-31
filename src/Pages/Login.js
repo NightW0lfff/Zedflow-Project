791cecb279ead329d3dbe9719c300608060c6e59
NightW0lfff <88699008+NightW0lfff@users.noreply.github.com>
2023-05-31 13:26:29 +1000

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,20 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./Login.css";
-import {
-  auth,
-  signInWithEmailAndPassword,
-  signInWithGoogle,
-} from "../firebase";
+import { auth, signInWithEmailAndPassword, signInWithGoogle } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, useNavigate } from "react-router-dom";
-import EmailIcon from "@mui/icons-material/Email";
-import LockIcon from "@mui/icons-material/Lock";
+import EmailIcon from '@mui/icons-material/Email';
+import LockIcon from '@mui/icons-material/Lock';
+
+
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
-  const [errMsg, setErrMsg] = useState("");
+  const [errMsg, setErrMsg] = useState('');
   const [success, setSuccess] = useState(false);
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
@@ -50,56 +49,45 @@ function Login() {
       });
   };
 
+
   return (
+
     <>
       {success ? (
         <div>
+
           <h1> Success!</h1>
           <p>
             <a href="./Components"></a>
           </p>
         </div>
       ) : (
+
         <div className="auth-form-container">
+
           <img
             src="/zedflow-logo.png"
             alt="Zedflow Logo"
             className="login-logo"
           />
 
-          <p className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
-            {errMsg}
-          </p>
+          <p className={errMsg ? "errmsg" :
+            "offscreen"} aria-live="assertive" >{errMsg}</p>
           <h2 style={{ color: "Black" }}>Welcome to Zedflow </h2>
           <form className="Login-form">
             <ul>
               <li>
-                <label for="email">
-                  {" "}
-                  <EmailIcon classname="emailIcon" />
-                </label>
-                <input
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  type="email"
-                  placeholder="Email"
-                  id="email"
-                  name="email"
-                />
+
+                <label for="email">  <EmailIcon classname="emailIcon" /></label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" id="email" name="email" />
               </li>
               <li>
                 <LockIcon classname="passIcon" />
                 <label for="password"> </label>
-                <input
-                  value={pass}
-                  onChange={(e) => setPass(e.target.value)}
-                  type="password"
-                  placeholder="********"
-                  id="password"
-                  name="password"
-                />
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
               </li>
             </ul>
+<<<<<<< HEAD
             <button
               type="submit"
               onClick={() => signInWithEmailAndPassword(email, pass)}
@@ -109,36 +97,40 @@ function Login() {
                 Sign In{" "}
               </a>{" "}
             </button>
+=======
+            <button type="submit" onClick={() => signInWithEmailAndPassword(email, pass)}>
+              <a href="/" style={{ color: "#FFFFFF" }}> Sign In </a> </button>
+>>>>>>> parent of 898ca1a20 (Update Login.js)
             <button className="login_google" onClick={handleGoogleSignIn}>
               Login with Google
             </button>
             <div>
-              <Link style={{ color: "#1E90FF" }} to={"/reset"}>
-                Forgot Password
-              </Link>
+              <Link style={{ color: "#1E90FF" }} to={"/reset"}>Forgot Password</Link>
             </div>
             <label>
-              <input
-                type="checkbox"
-                checked={rememberMe}
-                onChange={handleRememberMeChange}
-                name="remember"
-              />{" "}
-              Remember me
+              <input type="checkbox" checked={rememberMe} onChange={handleRememberMeChange} name="remember" /> Remember me
             </label>
           </form>
 
-          <p style={{ color: "#000000" }}>
+          <p style={{ color: "#000000" }} >
             Need an Account? <br />
             <span className="Signing">
-              <Link style={{ color: "#1E90FF" }} to={"/register"}>
-                Sign Up{" "}
-              </Link>
-            </span>
-          </p>
-        </div>
+              <Link style={{ color: "#1E90FF" }} to={"/register"}>Sign Up </Link>
+
+
+            </span >
+
+
+          </p >
+
+        </div >
       )}
     </>
-  );
+
+
+
+  )
+
 }
 export default Login;
+
