b8cc4f3e4882fcd69e9b821dc6150c1a66d96a42
NightW0lfff <philong170101@gmail.com>
2023-06-30 05:08:39 +1000

    Updated Code

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 import Reset from "./Pages/Reset.js";
 import Register from "./Pages/Register.js";
-import ProductList from "./Pages/ProductList/index"
+import ProductList from "./Pages/ProductList/index";
 import ImageMockup from "./Pages/ImageMockup";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/register" element={<Register />}></Route>
         <Route path="/reset" element={<Reset />} />
-        <Route path="/products/list" element={<ProductList  />}></Route>
+        <Route path="/products/list" element={<ProductList />}></Route>
         <Route path="/products/create" element={<ImageMockup />} />
       </Routes>
     </div>
